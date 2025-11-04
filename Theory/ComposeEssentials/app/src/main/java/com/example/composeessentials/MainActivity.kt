a725445cfd0852b14131c457ff3e0293fe8fd94e
RomaSorom <romasoromm@gmail.com>
2025-11-04 14:17:40 +0200

    Composable functions done

--- Theory/ComposeEssentials/app/src/main/java/com/example/composeessentials/MainActivity.kt
+++ Theory/ComposeEssentials/app/src/main/java/com/example/composeessentials/MainActivity.kt
@@ -14,9 +14,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -76,6 +78,29 @@ class MainActivity : ComponentActivity() {
                     // це і є "стан контролює ui"
                     // це називається перекомпозицією
 
+
+
+                    // перекомпозиція:
+                    // 1. перевиклик функції з новим параметром
+                    // 2. зміна внутрішнього стану функції (спричиняє 1 пункт у дочірніх елементах)
+
+                var state: MutableState<String?> = remember { mutableStateOf(null) }
+                // MutableState<T> - відстеження змін стану для перекомпозиції
+                // .value - зберігає значення стану
+                // remember - зберігає значення стану під час перекомпозиції
+                // rememberSaveable - зберігає значенняс стану навіть при зміні конфігурації (поворот екрану)
+                var state1: String by remember { mutableStateOf("String") }
+                // by - оператор делегування, делегує доступ до .value
+                // повертається не об'єкт MutableState, а значення, яке зберігається в .value
+
+
+                // composable функції можуть виконуватися у будь-якому порядку
+                    // пріоритетність обирає jetpack compose
+                // composable функції можуть виконуватися паралельно
+                // compose не перекомпозовує composable, які не взаємодіють зі станом
+                // поточна перекомпозиція може прерватися, якщо виникає портреба в новій з новими параметрами
+                // composable функція повинна виконуватися швидко, якщо вона буде виконуватися часто
+                    // (приклад: використання анімацій у елементі, який буде швидко та часто викликатися)
             }
         }
     }
@@ -103,7 +128,7 @@ fun Abc(num: Int, changeNum: (Int) -> Unit) {
 @Composable
 fun RecompositionTest() {
     Log.d("Compose test", "RecompositionTest was recomposed") // перекомпозовується, оскільки є взаємодія на рядку 112
-    var isBlack: Boolean by remember { mutableStateOf(true) }
+    var isBlack: Boolean by rememberSaveable { mutableStateOf(true) }
     Column(
         modifier = Modifier.padding(30.dp)
     ) {
